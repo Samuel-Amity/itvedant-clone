e4bb32d489143f0260e93eeb5c326cd72f25a4c2
Samuel Nathan <144701270+Samuel-Amity@users.noreply.github.com>
2025-02-05 04:53:46 +0530

    Frontend

--- itvedant_clone/courses/forms.py
+++ itvedant_clone/courses/forms.py
@@ -5,3 +5,8 @@ class ChapterForm(forms.ModelForm):
     class Meta:
         model = Chapter
         fields = ['course', 'title', 'content', 'question', 'answer', 'hint']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Apply Bootstrap class to form fields
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'    
